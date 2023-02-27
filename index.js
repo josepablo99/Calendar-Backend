dfaf2257b0bc74739f1409ae3d6cb428a23243a0
Jose Pablo <josepabloruizperez8@gmail.com>
2023-02-27 19:22:49 +0100

    Backend 1.0 terminado

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const { dbConnection } = require('./database/config');
+
+
+
+// Crear el servidor de express
+
+
+// Base de datos
+
+dbConnection();
+
+// CORS
+
+app.use(cors());
+
+// Directorio publico
+
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+
+app.use(express.json());
+
+// Rutas
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events')); 
+
+// Escuchar peticiones
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
---
+++ routes/events.js
@@ -0,0 +1,44 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const router = Router();
+
+const { actualizarEvento, crearEvento, eliminarEvento, getEventos } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+router.use(validarJWT);
+
+// Todas tienen que pasar la validacion del JWT
+// Obtener eventos
+
+router.get('/', getEventos);
+
+// Crear un nuevo evento
+
+router.post('/',
+  [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalizacion es obligatoria').custom(isDate),
+    validarCampos
+  ],
+  crearEvento);
+
+// Actualizar evento
+
+router.put('/:id',
+  [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalizacion es obligatoria').custom(isDate),
+    validarCampos
+  ],
+  actualizarEvento);
+
+// Borrar evento
+
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
