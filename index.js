6d7a8485944256eae0b851bde01558533a82909d
Jose Pablo <josepabloruizperez8@gmail.com>
2023-02-28 18:07:29 +0100

    commit de vercel

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const { dbConnection } = require('./database/config');
+const { createServer } = require('http')
 
 
 
@@ -31,8 +32,16 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events')); 
 
+// Manejar las rutas que no fueron manejadas por Express (esto es importante para Vercel)
+
+app.all('*', (req, res) => {
+  return handle(req, res);
+});
+
 // Escuchar peticiones
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
-});
+const server = createServer(app);
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+})
