7177be0be9149a138c318caf5ef67f49f577d50e
Jose Pablo <josepabloruizperez8@gmail.com>
2023-02-28 18:16:40 +0100

    mod

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const { dbConnection } = require('./database/config');
-const { createServer } = require('http')
 
 
 
@@ -32,16 +31,8 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events')); 
 
-// Manejar las rutas que no fueron manejadas por Express (esto es importante para Vercel)
-
-app.all('*', (req, res) => {
-  return handle(req, res);
-});
-
 // Escuchar peticiones
 
-const server = createServer(app);
-
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
